bcc192180c38f52c48834f912f5cd5f20e73b27f
alvindaledevera <alvindaledevera@gmail.com>
2026-01-05 11:22:53 +0800

    remove pacstraping systemd-networkd systemd-resolved

--- scripts/30-base.sh
+++ scripts/30-base.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-pacstrap /mnt base linux linux-firmware \
-btrfs-progs vim sudo git \
-systemd-networkd systemd-resolved \
+pacstrap /mnt base linux linux-firmware btrfs-progs vim sudo git nano \
 timeshift zram-generator
 
 genfstab -U /mnt >> /mnt/etc/fstab
