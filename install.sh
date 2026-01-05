6e884545086a9d422438a60329d9d6055e85e392
alvindaledevera <alvindaledevera@gmail.com>
2026-01-05 13:25:28 +0800

    make all exceutable

--- install.sh
+++ install.sh
@@ -5,11 +5,16 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 echo "=== ARCH LINUX INSTALLER ==="
 
-source "$SCRIPT_DIR/scripts/00-prompt.sh"
-source "$SCRIPT_DIR/scripts/01-checks.sh"
-source "$SCRIPT_DIR/scripts/10-luks-btrfs.sh"
-source "$SCRIPT_DIR/scripts/20-mount.sh"
-source "$SCRIPT_DIR/scripts/30-base.sh"
-source "$SCRIPT_DIR/scripts/40-chroot.sh"
+# Make all installer scripts executable (normal + chroot)
+chmod +x "$SCRIPT_DIR"/scripts/*.sh
+chmod +x "$SCRIPT_DIR"/scripts/chroot/*.sh
+
+# Source installer scripts
+source "$SCRIPT_DIR"/scripts/00-prompt.sh
+source "$SCRIPT_DIR"/scripts/01-checks.sh
+source "$SCRIPT_DIR"/scripts/10-luks-btrfs.sh
+source "$SCRIPT_DIR"/scripts/20-mount.sh
+source "$SCRIPT_DIR"/scripts/30-base.sh
+source "$SCRIPT_DIR"/scripts/40-chroot.sh
 
 echo "✅ Installation finished!"
