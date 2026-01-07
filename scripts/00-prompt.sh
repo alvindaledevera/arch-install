be7a05050580c5d5baca9dbce5e296475dc4ef2b
alvindaledevera <alvindaledevera@gmail.com>
2026-01-07 08:04:45 +0800

    Remove disk selection

--- scripts/00-prompt.sh
+++ scripts/00-prompt.sh
@@ -4,8 +4,8 @@ echo "=== ARCH LINUX INSTALLER ==="
 lsblk
 echo
 
-read -rp "Disk (e.g. nvme0n1 or vda): " DISK
-read -rp "EFI partition (e.g. nvme0n1p1 or vda1): " EFI_PART
+# Prompt only for EFI_PART and ARCH_PART
+read -rp "EFI partition (e.g. nvme0n1p1 or vda1 sda1): " EFI_PART
 read -rp "Arch partition (WILL BE ERASED): " ARCH_PART
 read -rp "Hostname: " HOSTNAME
 read -rp "Username: " USERNAME
@@ -13,7 +13,6 @@ read -rp "Timezone [Asia/Manila]: " TIMEZONE
 TIMEZONE=${TIMEZONE:-Asia/Manila}
 
 # Prefix /dev/ if missing
-[[ $DISK != /dev/* ]] && DISK="/dev/$DISK"
 [[ $EFI_PART != /dev/* ]] && EFI_PART="/dev/$EFI_PART"
 [[ $ARCH_PART != /dev/* ]] && ARCH_PART="/dev/$ARCH_PART"
 
