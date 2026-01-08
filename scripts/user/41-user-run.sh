e17760a255ecccfc10002fdf245017835aa960a7
alvindaledevera <alvindaledevera@gmail.com>
2026-01-09 04:23:26 +0800

    up

--- scripts/user/41-user-run.sh
+++ scripts/user/41-user-run.sh
@@ -17,7 +17,7 @@ if id "$USERNAME" &>/dev/null; then
     # Loop through all user scripts
     for script in /home/$USERNAME/user/*.sh; do
         [[ "$script" == *"41-user-run.sh"* ]] && continue  # skip this runner
-        source /home/$USERNAME/lib/ui.sh  # ensure print_stage is available
+        source /home/$USERNAME/script/lib/ui.sh  # ensure print_stage is available
         print_stage "$script"
         bash "$script"
     done
