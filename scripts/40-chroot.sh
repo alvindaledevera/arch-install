fd82a510d43f827e2d997dabbfb4c74f778d15b1
alvindaledevera <alvindaledevera@gmail.com>
2026-01-08 10:44:03 +0800

    copy install script to chroot

--- scripts/40-chroot.sh
+++ scripts/40-chroot.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env bash
 set -e
 
-# Export variables for chroot scripts
-export EFI_PART ARCH_PART HOSTNAME USERNAME KEYMAP LOCALE TIMEZONE
+# -----------------------------
+# Copy full arch-install folder into chroot
+# -----------------------------
+cp -r "$ROOT_DIR" /mnt/root/arch-install
 
-# Copy chroot scripts and lib folder
-cp -r "$ROOT_DIR/scripts/chroot" /mnt/root/chroot
-cp -r "$ROOT_DIR/scripts/lib" /mnt/root/chroot/lib
-
-# Run all chroot scripts numerically with automatic stage print
-for script in /mnt/root/chroot/[0-9][0-9]*.sh; do
-    arch-chroot /mnt /bin/bash -c "
-        source /root/chroot/lib/ui.sh
-        print_stage '$script'
-        bash '$script'
-    "
-done
-
-echo "✅ INSTALL COMPLETE"
+# Export variables for use inside chroot
+arch-chroot /mnt /bin/bash -c "export EFI_PART='$EFI_PART' \
+ARCH_PART='$ARCH_PART' \
+HOSTNAME='$HOSTNAME' \
+USERNAME='$USERNAME' \
+KEYMAP='$KEYMAP' \
+LOCALE='$LOCALE' \
+TIMEZONE='$TIMEZONE' && \
+bash -c 'for script in /root/arch-install/scripts/chroot/[0-9][0-9]*.sh; do
+    source /root/arch-install/scripts/lib/ui.sh
+    print_stage \$script
+    source \$script
+done'"
