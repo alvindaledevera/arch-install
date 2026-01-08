b8ebfe455ce773ccedf628bd6a41a0ce383a0e59
alvindaledevera <alvindaledevera@gmail.com>
2026-01-08 10:52:25 +0800

    update symb link condition

--- scripts/chroot/43-network.sh
+++ scripts/chroot/43-network.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
+set -e
 
+# Enable networking services
 systemctl enable systemd-networkd
 systemctl enable systemd-resolved
 
-ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
+# Only create symlink if it doesn't exist or points somewhere else
+if [ "$(readlink -f /etc/resolv.conf)" != "/run/systemd/resolve/stub-resolv.conf" ]; then
+    ln -sf /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
+fi
 
+# Create basic DHCP config for wired interfaces
 cat <<EOF > /etc/systemd/network/20-wired.network
 [Match]
 Name=en*
