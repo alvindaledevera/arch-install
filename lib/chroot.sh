7c47850fc0e6fda3766d2ffd29522be6aab5715f
alvindaledevera <alvindaledevera@gmail.com>
2026-01-19 05:09:58 +0800

    try

--- lib/chroot.sh
+++ lib/chroot.sh
@@ -1,13 +1,13 @@
 run_copy_install_script(){
     # Copy  scripts into the new system
-cp -r "$ROOT_DIR" /mnt/root/$ROOT_DIR
+cp -r "$ROOT_DIR" "/mnt/root/arch-install"
 
 }
 
 
 run_chroot(){
 # Run all chroot scripts numerically
-for script in /mnt/root/$ROOT_DIR/04-configure-system/[0-9][0-9]*.sh; do
-    arch-chroot /mnt "/root/$ROOT_DIR/$(basename "$script")"
+for script in /mnt/root/arch-install/04-configure-system/[0-9][0-9]*.sh; do
+    arch-chroot /mnt "/root/arch-install/04-configure-system/$(basename "$script")"
 done    
 }
