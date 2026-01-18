092f8f1721ec7c096b6d6ef1d1d39d05dae159bc
alvindaledevera <alvindaledevera@gmail.com>
2026-01-19 05:15:51 +0800

    update chroot

--- lib/chroot.sh
+++ lib/chroot.sh
@@ -5,9 +5,14 @@ cp -r "$ROOT_DIR" "/mnt/root/arch-install"
 }
 
 
-run_chroot(){
-# Run all chroot scripts numerically
-for script in /mnt/root/arch-install/04-configure-system/[0-9][0-9]*.sh; do
-    arch-chroot /mnt "/root/arch-install/04-configure-system/$(basename "$script")"
-done    
+run_chroot() {
+    arch-chroot /mnt /bin/bash -euo pipefail <<'EOF'
+# Make sure scripts are executable
+chmod +x /root/arch-install/04-configure-system/*.sh
+
+# Run scripts in order
+for script in /root/arch-install/04-configure-system/[0-9][0-9]*.sh; do
+    /bin/bash "$script"
+done
+EOF
 }
