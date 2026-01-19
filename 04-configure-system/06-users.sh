13aab8ec375e8346786d84e24e439549b589fc28
alvindaledevera <alvindaledevera@gmail.com>
2026-01-19 08:16:14 +0800

    try

--- 04-configure-system/06-users.sh
+++ 04-configure-system/06-users.sh
@@ -6,31 +6,24 @@ ui_banner "User Setup"
 # -----------------------------
 # Root password
 # -----------------------------
-ui_section "Set root password"
-echo "Please enter the root password:"
-passwd root
+ui_info "Setting root password"
+echo "root:${ROOT_PASSWORD}" | chpasswd
 
 # -----------------------------
-# Create regular user
+# User creation
 # -----------------------------
-read -rp "Enter your username: " USERNAME
-
-ui_info "Creating user: $USERNAME"
-useradd -m -G wheel -s /bin/bash "$USERNAME"
-
-# -----------------------------
-# Set user password
-# -----------------------------
-ui_section "Set password for $USERNAME"
-echo "Please enter password for user $USERNAME:"
-passwd "$USERNAME"
-
-# -----------------------------
-# Sudo setup (wheel group)
-# -----------------------------
-ui_info "Ensuring wheel group has sudo privileges"
-if ! grep -q "^%wheel" /etc/sudoers; then
-    echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
+if ! id "$USERNAME" &>/dev/null; then
+    ui_info "Creating user $USERNAME"
+    useradd -m -G wheel "$USERNAME"
 fi
 
-ui_success "User $USERNAME created and configured successfully"
+ui_info "Setting password for $USERNAME"
+echo "${USERNAME}:${USER_PASSWORD}" | chpasswd
+
+# -----------------------------
+# Sudo access
+# -----------------------------
+ui_info "Enabling sudo for wheel group"
+sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
+
+ui_success "Users configured successfully"
